14c800c49a704db7ca41271313242051531d3707
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-03-19 20:33:23 +0200

    fixed-filter-return

--- backend/filter.php
+++ backend/filter.php
@@ -7,10 +7,10 @@ $departure_date = $_GET['departure_date'];
 $return_date = $_GET['return_date'];
 
 if(!empty($_GET["return_date"])){
-    $query = $mysqli->prepare('SELECT distinct * FROM flights f right Join airports a on a.id=f.arrival_airport_id or a.id=departure_airport_id WHERE (departure_airport_id = ? AND arrival_airport_id = ? AND DATE(departure_date) = ?) OR (departure_airport_id = ? AND arrival_airport_id = ? AND departure_date = ?)');
+    $query = $mysqli->prepare('SELECT * FROM flights f join airports a on a.id=departure_airport_id join airports a2 on a2.id=arrival_airport_id WHERE (departure_airport_id = ? AND arrival_airport_id = ? AND DATE(departure_date) = ?) OR (departure_airport_id = ? AND arrival_airport_id = ? AND DATE(departure_date) = ?)');
     $query->bind_param('iisiis', $from_airport, $to_airport, $departure_date, $to_airport, $from_airport, $return_date);
 }else{
-    $query = $mysqli->prepare('SELECT distinct * from flights f right join airports a on a.id=f.arrival_airport_id or a.id=departure_airport_id where departure_airport_id = ? and arrival_airport_id = ? and date(departure_date) = ?');
+    $query = $mysqli->prepare('SELECT * from flights f join airports a on a.id=departure_airport_id join airports a2 on a2.id=arrival_airport_id where departure_airport_id = ? and arrival_airport_id = ? and date(departure_date) = ?');
     $query->bind_param('iis', $from_airport, $to_airport, $departure_date);
 
 }
@@ -27,7 +27,7 @@ if($num_rows == 0) {
     $response["message"] = "No flights to show";
 }else{
     $flights = [];
-    $query->bind_result($flight_id, $airplane_id, $departure_airport_id, $arrival_airport_id, $departure_date, $arrival_date, $flight_status, $price, $airport_id, $code, $name, $city, $country,);
+    $query->bind_result($flight_id, $airplane_id, $departure_airport_id, $arrival_airport_id, $departure_date, $arrival_date, $flight_status, $price, $departure_airport_id, $departure_code, $departure_name, $departure_city, $departure_country, $arrival_airport_id, $arrival_code, $arrival_name, $arrival_city, $arrival_country);
     while ($query->fetch()) {
         $flight = [
             'id' => $flight_id,
@@ -35,8 +35,10 @@ if($num_rows == 0) {
             'departure_date' => $departure_date,
             'arrival_date' => $arrival_date,
             'price' => $price,
-            'code' => $code,
-            'country' => $country
+            'departure_code' => $departure_code,
+            'departure_country' => $departure_country,
+            'arrival_code' => $arrival_code,
+            'arrival_country' => $arrival_country
         ];
         $flights[] = $flight;
         $response['status'] = 'success';
--- frontend/scripts/filter.js
+++ frontend/scripts/filter.js
@@ -3,18 +3,32 @@ const resultsSection = document.getElementById("results");
 const resultsTitle = document.getElementById("results-title");
 const adsSection = document.getElementById("ads-section");
 const adsTitle = document.getElementById("ads-title");
+let waysInput = document.getElementById("type-input");
+const returnDate = document.getElementById("return-date");
+let ways = waysInput.options[waysInput.selectedIndex].value;
+
+waysInput.addEventListener('change', ()=>{
+  ways = waysInput.options[waysInput.selectedIndex].value;
+  if(ways == "One way"){
+    returnDate.disabled = true;
+  }
+  else{
+    returnDate.disabled = false;
+  }
+})
+
 
 
 searchButton.addEventListener('click', ()=>{
     const fromInput = document.getElementById("from-input");
     const toInput = document.getElementById("to-input");
     const departureDate = document.getElementById("departure-date").value;
-    const returnDate = document.getElementById("return-date").value;
-    const waysInput = document.getElementById("type-input")
+    returnDate = document.getElementById("return-date").value;
+    waysInput = document.getElementById("type-input")
     
     const airportFrom = fromInput.options[fromInput.selectedIndex].value;
     const airportTo = toInput.options[toInput.selectedIndex].value;
-    const ways = waysInput.options[waysInput.selectedIndex].value;
+    ways = waysInput.options[waysInput.selectedIndex].value;
 
     adsSection.classList.add("hidden")
     adsTitle.classList.add("hidden")
@@ -25,15 +39,15 @@ searchButton.addEventListener('click', ()=>{
 
     axios.get(`http://localhost/Team 4 - flight system/backend/filter.php?from_airport=${airportFrom}&to_airport=${airportTo}&departure_date=${departureDate}&return_date=${returnDate}`)
   .then(response => {
+        const flights = response.data.flights;
+        resultsSection.innerHTML = '';
         if(ways == "One way"){
-          const flights = response.data.flights;
-          console.log(flights)
-          resultsSection.innerHTML = '';
+
           flights.forEach(element => {
             
             let deprt_date = moment(element.departure_date).format('YYYY-MM-DD HH:mm')
             let arriv_date = moment(element.arrival_date).format('YYYY-MM-DD HH:mm')
-            resultsSection.innerHTML += `<div class="booking-card flex justify-between ">
+            resultsSection.innerHTML += `<div class="booking-card flex justify-between align center ">
             <div>
                 <i class="fa-solid fa-plane-departure"></i>
             </div>
@@ -44,7 +58,7 @@ searchButton.addEventListener('click', ()=>{
                 <p>Non stop</p>
             </div>
             <div>
-                <p>${element.code} - ${element.country}</p>
+                <p>${element.departure_code} - ${element.arrival_code}</p>
             </div>
             <div class="line"></div>
             <div>
@@ -54,14 +68,55 @@ searchButton.addEventListener('click', ()=>{
             </div>
         </div>`  
 
-          
-
-
-
-
               });
 
-        }
-    });
+        }else{
+          flights.forEach(element => {
+            
+            let deprt_date = moment(element.departure_date).format('YYYY-MM-DD HH:mm')
+            let arriv_date = moment(element.arrival_date).format('YYYY-MM-DD HH:mm')
 
+            const matchedFlight = flights.filter(matchedItem => matchedItem.departure_code == element.arrival_code && matchedItem.arrival_code == element.departure_code);
+            let foundFlight = matchedFlight[0]
+            flights.splice(0, 1)
+
+            let matched_deprt_date = moment(foundFlight.departure_date).format('YYYY-MM-DD HH:mm')
+            let matched_arriv_date = moment(foundFlight.arrival_date).format('YYYY-MM-DD HH:mm')
+
+
+            resultsSection.innerHTML += `<div class="booking-card flex justify-between ">
+            <div>
+                <i class="fa-solid fa-plane-departure"></i>
+                <br>
+                <br>
+                <i class="fa-solid fa-plane-arrival"></i>
+            </div>
+            <div>
+                <p>${deprt_date} - ${arriv_date}</p>
+                <br>
+                <p>${matched_deprt_date} - ${matched_arriv_date}</p>
+            </div>
+            <div>
+                <p>Non stop</p>
+                <br>
+                <p>Non stop</p>
+
+            </div>
+            <div>
+                <p>${element.departure_code} - ${element.arrival_code}</p>
+                <br>
+                <p>${foundFlight.departure_code} - ${foundFlight.arrival_code}</p>
+
+            </div>
+            <div class="line"></div>
+            <div>
+                <p>${element.price}</p>
+                <p>Economy</p>
+                <button class="btn-style btn" id="flight-btn">Learn more</button>
+            </div>
+        </div>`  
+        });
+    };
+
+  })
 })
