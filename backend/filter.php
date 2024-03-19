55bdddcf0043f123b7c1cf896c42f0ac9497693f
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-03-19 15:02:51 +0200

    added-header-folder

--- backend/filter.php
+++ backend/filter.php
@@ -1,22 +1,32 @@
 <?php
 include('connection.php');
 
-$ways = $_GET['ways'];
 $from_airport = $_GET['from_airport'];
 $to_airport = $_GET['to_airport'];
 $departure_date = $_GET['departure_date'];
 $return_date = $_GET['return_date'];
 
 if(!empty($_GET["return_date"])){
-    $query = $mysqli->prepare('select * from flights where (departure_airport_id = ? arrival_airport_id = ? departure_date = ?) or (departure_airport_id = ? arrival_airport_id = ? departure_date = ?) ');
+    $query = $mysqli->prepare('select * from flights where (departure_airport_id = ? and arrival_airport_id = ? and date(departure_date) = ?) or (departure_airport_id = ? and arrival_airport_id = ? and departure_date = ?) ');
     $query->bind_param('iisiis', $from_airport, $to_airport, $departure_date, $to_airport, $from_airport, $return_date);
-    $query->store_result();
+
+
 }else{
-    $query = $mysqli->prepare('select * from flights where departure_airport_id = ? arrival_airport_id = ? departure_date = ?');
+    $query = $mysqli->prepare('select * from flights where departure_airport_id = ? and arrival_airport_id = ? and date(departure_date) = ?');
     $query->bind_param('iis', $from_airport, $to_airport, $departure_date);
-    $query->store_result();
+
 }
 
+$logString = $sql;
+foreach ($_GET as $key => $value) {
+    $logString = str_replace("?", "'$value'", $logString);
+}
+echo "SQL Query: $logString\n";
+
+
+
+$query->execute();
+$query->store_result();
 $num_rows = $query->num_rows();
 if($num_rows == 0) {
     $response["status"] = "No flights";
@@ -35,7 +45,6 @@ if($num_rows == 0) {
         $response['flights'] = $flights;
     }};
 
-$response
 echo json_encode($response);
 
 
