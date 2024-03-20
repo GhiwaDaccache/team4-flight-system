99f99be6cda91e0b385a96f1e5b152d9f0e5cfbe
Rafik Hijazy <rafikhz90@gmail.com>
2024-03-20 15:51:01 +0200

    Finish setmessages

---
+++ backend/sendmessage.php
@@ -0,0 +1,10 @@
+<?php
+include("messages.php");
+$receiver_id=$_POST["id"];
+$chat_id=$_POST["chat_id"];
+$sender_id=$_POST["sender_id"];
+$message=$_POST["message"];
+$query=$mysqli->prepare("insert into messages(id,chat_id,sender_id,message_text) value(?,?,?,?)");
+$query->bind_param('ssss',$receiver_id,$chat_id,$sender_id,$message);
+$query->execute();
+$response["response"]="sended";
