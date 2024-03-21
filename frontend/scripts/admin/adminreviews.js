549b70351add6f92d15c8025c0aac4541b6f8697
Rafik Hijazy <rafikhz90@gmail.com>
2024-03-21 07:14:41 +0200

    Finish adminreviews full-stack

--- frontend/scripts/admin/adminreviews.js
+++ frontend/scripts/admin/adminreviews.js
@@ -1,5 +1,6 @@
 const user_data=localStorage.getItem("credentials");
-
+const reviews_table=document.querySelector("#reviews");
+const get_reviews_url="http://localhost/flight-agency-project/backend/getfeedbacks.php";
 
 const setUserData=(user)=>{
     console.log(user)
@@ -8,4 +9,20 @@ const setUserData=(user)=>{
     name.innerHTML=JSON.parse(user).username
 }
 }
-setUserData(user_data)
+setUserData(user_data);
+const fillReviewsTable=async (url)=>{
+    const result =await axios.get(url);
+    const {data}=await result;
+    const {reviews}=data
+    reviews.forEach(element => {
+        const{user_id,flight_id,review_text,rating}=element
+        reviews_table.innerHTML+=`<tr>
+        <td>${review_text}</td>
+        <td>user${user_id}</td>
+        <td>${new Date().getDay()}:${new Date().getMonth()}:${new Date().getFullYear()}</td>
+        <td>${flight_id}</td>
+        <td>${rating}</td>
+        </tr>`
+    });
+}
+fillReviewsTable(get_reviews_url)
