5d106e849b3ee824dae3549a3b8939a47ae884a7
Youssof Hammoud <youssofhammoud@gmail.com>
2024-03-21 10:26:20 +0200

    send message added date sent

--- backend/sendmessage.php
+++ backend/sendmessage.php
@@ -4,8 +4,10 @@ include("messages.php");
 $chat_id=$_POST["chat_id"];
 $sender_id=$_POST["sender_id"];
 $message=$_POST["message"];
-$query=$mysqli->prepare("insert into messages(chat_id,sender_id,message_text) value(?,?,?)");
-$query->bind_param('sss',$chat_id,$sender_id,$message);
+$date=date("Y-m-d H:i:s");
+
+$query=$mysqli->prepare("insert into messages(chat_id,sender_id,message_text,sent_at) value(?,?,?,?)");
+$query->bind_param('ssss',$chat_id,$sender_id,$message,$date);
 $query->execute();
 $response["response"]="sended";
 echo json_encode($response);
